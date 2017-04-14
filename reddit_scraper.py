73757511e0512ac3c1527641f5ea1e3942d4d091
Trevor Nelson <teafiend9@gmail.com>
2017-04-13 18:59:05 -0700

    Added .gitignore and functionality for reading praw config from a file.

--- reddit_scraper.py
+++ reddit_scraper.py
@@ -1,5 +1,7 @@
 import praw
 
+PRAW_INIT_FILE_NAME = 'praw_init.txt'
+
 class PrawInitData:
 	def __init__(self, client_id, client_secret, password, user_agent, username):
 		self.client_id = client_id
@@ -12,5 +14,16 @@ def make_praw_instance(init_data):
 	return praw.Reddit(init_data.client_id, init_data.client_secret, init_data.password,
 						 init_data.user_agent, init_data.username)
 
-def get_hot_post_titles(reddit, n):
+def get_titles_from_praw(reddit, n):
 	return [submission.title for submission in reddit.front.hot(limit=n)]
+
+def initialize_praw(file_name):
+	file = open(file_name)
+	file_line_list = tuple([line for line in file])
+	return PrawInitData(*file_line_list)
+
+
+def get_hot_post_titles(n):
+	praw_init_fields = initialize_praw(PRAW_INIT_FILE_NAME)
+	reddit = make_praw_instance(praw_init_fields)
+	return get_titles_from_praw(reddit, n)
