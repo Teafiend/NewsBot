7bd5b377f24c12810127df88a3876b60dfbc33ed
Trevor Nelson <teafiend9@gmail.com>
2017-04-13 18:42:04 -0700

    Added functionality for parsing RSS feeds from Google.

---
+++ google_feed_parser.py
@@ -0,0 +1,19 @@
+import feedparser
+import urllib.parse
+
+GOOGLE_ROOT_URL = "https://news.google.com/?"
+
+def encode_url(query):
+	param_dict = {"output": "rss", "q": query}
+	return GOOGLE_ROOT_URL + urllib.parse.urlencode(param_dict)
+
+def parse_query(url):
+	return feedparser.parse(url)
+
+def get_titles_and_links(query):
+	url = encode_url(query)
+	parsed_data = parse_query(url)
+	return_list = []
+	for entry in parsed_data['entries']:
+		return_list.append((entry['title'], entry['link']))
+	return return_list
